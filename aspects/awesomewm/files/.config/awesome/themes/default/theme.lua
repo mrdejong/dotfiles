655e99fbd0138a5996f8cca3d609ce0e63622760
Alexander de Jong <mrdejong89@gmail.com>
2021-12-03 18:16:47 +0100

    Disable the ugly squares
    - These squares identify if there is a app running in one of the tags.
      I usually remember it, am smart enought todo so :P

--- aspects/awesomewm/files/.config/awesome/themes/default/theme.lua
+++ aspects/awesomewm/files/.config/awesome/themes/default/theme.lua
@@ -77,12 +77,12 @@ theme.tasklist_disable_icon    = true
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
-theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
-    taglist_square_size, theme.fg_normal
-)
-theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
-    taglist_square_size, theme.fg_normal
-)
+-- theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+--     taglist_square_size, theme.fg_normal
+-- )
+-- theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+--     taglist_square_size, theme.fg_normal
+-- )
 
 -- Variables set for theming notifications:
 -- notification_font
